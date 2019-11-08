11eb6cc9eb2d2943d32a230c5acfc05d4db60e85
adrianhuebner <adrianh92@gmail.com>
2019-11-07 22:02:08 -0800

    uml added

--- app/server.js
+++ app/server.js
@@ -6,3 +6,8 @@ io.on('connection', (socket) => {
   console.log('Connected', socket.id);
   socket.on('speak', payload => io.emit('message', payload));
 });
+
+io.on('write', (socket) => {
+  console.log('Write got where it needs', socket.id);
+  socket.on('file-save', payload => io.emit('file saved', payload))
+});
